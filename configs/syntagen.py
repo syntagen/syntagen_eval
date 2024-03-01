dcfedecdab4938be880a5b1789d7a559cca5621f
truongvu2000nd <truongvu0911nd@gmail.com>
2024-03-01 14:38:05 +0700

    save best ckpt

--- configs/syntagen.py
+++ configs/syntagen.py
@@ -154,7 +154,7 @@ optim_wrapper = dict(
     ),
 )
 # learning policy
-param_scheduler = [dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=20000, by_epoch=False)]
+param_scheduler = [dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=10000, by_epoch=False)]
 # training schedule for 10k
 train_cfg = dict(type='IterBasedTrainLoop', max_iters=10000, val_interval=1000)
 val_cfg = dict(type='ValLoop')
@@ -163,6 +163,6 @@ default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=1000),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=1000, save_best="mIoU"),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='SegVisualizationHook'))
